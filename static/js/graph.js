ee235f840d43ac8e90b7b6b2ca8852dc53f98a54
Micaela Signorelli <m.signorelli@gmail.com>
2018-06-24 14:56:29 +0000

    graph commenced

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,6 +3,26 @@ queue()
     .await(makeGraphs);
     
 function makeGraphs(error, victimsData) {
-
+    var ndx = crossfilter(victimsData);
+        
+    show_gender(ndx);
+        
+    dc.renderAll();
+}
+function show_gender(ndx) {
+    var dim = ndx.dimension(dc.pluck('Sex'));
+    var group = dim.group();
     
+    dc.barChart("#gender")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
 }
