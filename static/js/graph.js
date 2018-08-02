389b08a67be0e453bf4af889cea44138c9b422a5
Micaela Signorelli <m.signorelli@gmail.com>
2018-08-02 09:27:22 +0000

    region graph added

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,6 +8,8 @@ function makeGraphs(error, ChileData) {
     show_gender_selector(ndx);    
     show_vote(ndx);
     show_income(ndx);
+    show_region(ndx);
+    // show_statusquo(ndx);
    // show_income_to_vote_correlation(ndx);
         
     dc.renderAll();
@@ -58,6 +60,45 @@ function show_income(ndx) {
         .xAxisLabel('Income')
         .yAxis().ticks(20);
 }
+
+function show_region(ndx) {
+    var dim = ndx.dimension(dc.pluck('region'));
+    var group = dim.group();
+    
+    dc.barChart("#region")
+        .width(400)
+        .height(500)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(300)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Region")
+        .yAxis().ticks(20);
+}
+function show_statusquo(ndx) {
+    var dim = ndx.dimension(dc.pluck('statusquo'));
+    var group = dim.group();
+    
+    dc.barChart("#vote")
+        .width(400)
+        .height(500)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(300)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Support level of statusquo")
+        .yAxis().ticks(20);
+}
+
+
+
+
 
 /** function show_income_to_vote_correlation(ndx) {
     var genderColors = d3.scale.ordinal()
