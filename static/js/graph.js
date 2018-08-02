255cb1cf9f213aefbdd30a49ea66c29a696ac575
Micaela Signorelli <m.signorelli@gmail.com>
2018-08-02 11:07:17 +0000

    education graph added

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,24 +1,25 @@
 queue()
     .defer(d3.csv, "data/Chile.csv")
     .await(makeGraphs);
-    
+
 function makeGraphs(error, ChileData) {
     var ndx = crossfilter(ChileData);
-    
-    show_gender_selector(ndx);    
+
+    show_gender_selector(ndx);
     show_vote(ndx);
     show_income(ndx);
     show_region(ndx);
-//    show_statusquo(ndx);
-        
+    show_education(ndx);
+    //    show_statusquo(ndx);
+
     dc.renderAll();
-    
+
 }
 
 function show_gender_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('sex'));
     var group = dim.group();
-    
+
     dc.selectMenu("#gender_selector")
         .dimension(dim)
         .group(group);
@@ -27,11 +28,11 @@ function show_gender_selector(ndx) {
 function show_vote(ndx) {
     var dim = ndx.dimension(dc.pluck('vote'));
     var group = dim.group();
-    
+
     dc.barChart("#vote")
         .width(400)
         .height(500)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(300)
@@ -47,9 +48,9 @@ function show_income(ndx) {
     var group = dim.group();
 
     dc.barChart('#income')
-        .width(400)
-        .height(500)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .width(500)
+        .height(400)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(300)
@@ -63,11 +64,11 @@ function show_income(ndx) {
 function show_region(ndx) {
     var dim = ndx.dimension(dc.pluck('region'));
     var group = dim.group();
-    
+
     dc.barChart("#region")
-        .width(400)
-        .height(500)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .width(300)
+        .height(400)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(300)
@@ -78,20 +79,20 @@ function show_region(ndx) {
         .yAxis().ticks(20);
 }
 
-function show_statusquo(ndx) {
-    var dim = ndx.dimension(dc.pluck('statusquo'));
+function show_education(ndx) {
+    var dim = ndx.dimension(dc.pluck('education'));
     var group = dim.group();
-    
-    dc.barChart("#vote")
-        .width(400)
-        .height(500)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+
+    dc.barChart("#education")
+        .width(300)
+        .height(400)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(300)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("Support of statusquo")
+        .xAxisLabel("Education")
         .yAxis().ticks(20);
 }
