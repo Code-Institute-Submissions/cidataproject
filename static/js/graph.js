5bae752fca0a410694e9f527603c47ebe17fd3b9
Micaela Signorelli <m.signorelli@gmail.com>
2018-08-09 15:49:27 +0000

    scatter graph work in progress

--- static/js/graph.js
+++ static/js/graph.js
@@ -124,4 +124,38 @@ function show_education(ndx) {
         .elasticY(true)
         .xAxisLabel("Education")
         .yAxis().ticks(20);
-};
+}
+
+function show_age_to_vote_correlation(ndx) {
+   var voteColors = d3.scale.ordinal()
+       .domain(["Y", "N", "A", "U", "NA"])
+       .range(["red", "blue", "yellow", "purple", "pink"]);
+
+   var eDim = ndx.dimension(dc.pluck("age"));
+   var ageDim = ndx.dimension(function(d){
+       return [d.age];
+   });
+   var age = ageDim.group();
+
+   var minAge = eDim.bottom(1)[0].age;
+   var maxAge = eDim.top(1)[0].age;
+
+   dc.scatterPlot("#age_to_vote_correlation")
+       .width(800)
+       .height(400)
+       .x(d3.scale.linear().domain([minAge,maxAge]))
+       .brushOn(false)
+       .symbolSize(8)
+       .clipPadding(10)
+       .yAxisLabel("Vote")
+       .xAxisLabel("Age")
+       .title(function (d) {
+           return d.key[2] + " earned " + d.key[1];
+       })
+       .colorAccessor(function (d) {
+           return d.key[3];
+       })
+       .colors(voteColors)
+       .dimension(ageDim)
+       .group(age)
+       .margins({top: 10, right: 50, bottom: 75, left: 75});
