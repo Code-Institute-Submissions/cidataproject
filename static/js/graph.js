2d47566a9e3b9e3c20b32757d8f4231ac717aebb
Micaela Signorelli <m.signorelli@gmail.com>
2018-08-13 09:50:21 +0000

    new bootswatch added

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,5 +1,5 @@
 queue()
-    .defer(d3.csv, "data/Chile.csv")
+    .defer(d3.csv, 'data/Chile.csv')
     .await(makeGraphs);
 
 function makeGraphs(error, ChileData) {
@@ -22,7 +22,7 @@ function makeGraphs(error, ChileData) {
     var dim = ndx.dimension(dc.pluck('sex'));
     var group = dim.group();
 
-    dc.selectMenu("#gender_selector")
+    dc.selectMenu('#gender_selector')
         .dimension(dim)
         .group(group);
 } 
@@ -31,7 +31,7 @@ function show_vote_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('vote'));
     var group = dim.group();
 
-    dc.selectMenu("#vote_selector")
+    dc.selectMenu('#vote_selector')
         .dimension(dim)
         .group(group);
 } **/
@@ -40,9 +40,9 @@ function show_vote(ndx) {
     var dim = ndx.dimension(dc.pluck('vote'));
     var group = dim.group();
 
-    dc.barChart("#vote")
-        .width(800)
-        .height(300)
+    dc.barChart('#vote')
+        .width(600)
+        .height(250)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
@@ -50,7 +50,7 @@ function show_vote(ndx) {
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("Vote")
+        .xAxisLabel('Vote')
         .yAxis().ticks(20);
 }
 
@@ -87,13 +87,13 @@ function show_region(ndx) {
     var dim = ndx.dimension(dc.pluck('region'));
     var group = dim.group();
 
-    dc.rowChart("#region")
+    dc.rowChart('#region')
         .width(300)
         .height(250)
         .margins({ top: 30, right: 50, bottom: 20, left: 50 })
         .dimension(dim)
         .group(group)
-        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
+        .ordinalColors(['#CACAE3', '#6BAED6', '#9ecae1', '#1F77B4', '#7AC0F0'])
         .transitionDuration(300)
         .elasticX(true)
 }
@@ -115,7 +115,7 @@ function show_age(ndx) {
     var dim = ndx.dimension(dc.pluck('age'));
     var group = dim.group();
 
-    dc.barChart("#age")
+    dc.barChart('#age')
         .width(1000)
         .height(200)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
@@ -124,8 +124,8 @@ function show_age(ndx) {
         .transitionDuration(300)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .yAxisLabel("Count")
-        .xAxisLabel("Age")
+        .yAxisLabel('Count')
+        .xAxisLabel('Age')
         .elasticY(true)
         .elasticX(true);
 
